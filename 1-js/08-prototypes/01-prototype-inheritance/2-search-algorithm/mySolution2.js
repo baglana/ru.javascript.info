5fe0e0e441532c12cf9fb515909729f0cf02200e
baglana <102351104+baglana@users.noreply.github.com>
2025-09-03 13:33:51 +0500

    Add 1-js/08-prototypes/01-prototype-inheritance/ solutions

---
+++ 1-js/08-prototypes/01-prototype-inheritance/2-search-algorithm/mySolution2.js
@@ -0,0 +1,28 @@
+let head = {
+  glasses: 1,
+};
+
+let table = {
+  pen: 3,
+};
+
+let bed = {
+  sheet: 1,
+  pillow: 2,
+};
+
+let pockets = {
+  money: 2000,
+};
+
+Object.setPrototypeOf(pockets, bed);
+Object.setPrototypeOf(bed, table);
+Object.setPrototypeOf(table, head);
+
+console.log(pockets.pen);
+console.log(bed.glasses);
+console.log(table.money);
+
+
+// head.glasses
+// pockets → bed → table → head
---
+++ 1-js/08-prototypes/01-prototype-inheritance/4-hamster-proto/mySolution2.js
@@ -0,0 +1,25 @@
+let hamster = {
+  stomach: ['h'],
+
+  eat(food) {
+    this.stomach.push(food);
+    // this.stomach = [...this.stomach, food];
+  },
+};
+
+let speedy = {
+  // stomach: [],
+  __proto__: hamster,
+};
+
+let lazy = {
+  // stomach: [],
+  __proto__: hamster,
+};
+
+// Этот хомяк нашёл еду
+speedy.eat("apple");
+console.log(speedy.stomach); // apple
+
+// У этого хомяка тоже есть еда. Почему? Исправьте
+console.log(lazy.stomach); // apple
